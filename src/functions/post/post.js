8dc8d7c70e11ad581a114e8af7bae10ddfe6fd95
JelleRouquart <jelle.rouquart@student.howest.be>
2020-05-06 00:19:05 +0200

    testing

--- src/functions/post/post.js
+++ src/functions/post/post.js
@@ -4,11 +4,37 @@
 const shortid = require("shortid")
 const querystring = require("querystring")
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const client = contentful.createClient({
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+})
+
 exports.handler = async function(event, context) {
   try {
       // destructuren van 'link' post data
     const vuistje = querystring.parse(event.body);
     vuistje.id = shortid.generate();
+
+    const space = await client.getSpace(process.env.CONTENTFUL_SPACE_ID)
+    const environment = await space.getEnvironment("master")
+    const entry = await environment.createEntryWithId("vuist", vuistje.id, {
+      fields: {
+        to: {
+          "en-US": vuistje.to,
+        },
+        from: {
+          "en-US": vuistje.from,
+        },
+        message: {
+          "en-US": vuistje.message,
+        },
+      },
+    })
+
+    await entry.publish();
 
     return {
       statusCode: 200,
