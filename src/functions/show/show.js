2de619df16fa131bd269cfc3a5e6896c630e9fa9
JelleRouquart <jelle.rouquart@student.howest.be>
2020-05-06 09:39:58 +0200

    build cash

--- src/functions/show/show.js
+++ src/functions/show/show.js
@@ -13,27 +13,34 @@ const storage = new Map()
 exports.handler = async function(event, context) {
   try {
     const id = event.queryStringParameters.id.replace("/", "")
-    
-    const space = await client.getSpace(process.env.CONTENTFUL_SPACE_ID)
-    const environment = await space.getEnvironment("master")
-    const entry = await environment.getEntry(id)
+    let vuistje;
 
-    const vuistje = {
-      to: entry.fields.to["en-US"],
-      from: entry.fields.from["en-US"],
-      message: entry.fields.message["en-US"],
+    if (storage.has(id)) {
+      console.log("in storrage gevonden");
+      vuistje = storage.get(id)
+    } else {
+      const space = await client.getSpace(process.env.CONTENTFUL_SPACE_ID)
+      const environment = await space.getEnvironment("master")
+      const entry = await environment.getEntry(id)
+      
+      vuistje = {
+        to: entry.fields.to["en-US"],
+        from: entry.fields.from["en-US"],
+        message: entry.fields.message["en-US"],
+      }
+      console.log("In storrage plaatsen");
+      storage.set(id, vuistje)
     }
 
     return {
       statusCode: 200,
       body: JSON.stringify(vuistje),
     }
-
   } catch (err) {
     console.log(err) // output to netlify function log
     return {
       statusCode: 500,
-      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: err.message }), // Could be a custom message or object i.e. JSON.stringify(err)
     }
   }
 }
